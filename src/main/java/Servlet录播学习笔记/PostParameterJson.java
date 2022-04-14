331aba6ae28b480c8cfadd1af029532aca0d1e86
ningxia2021 <2424917250@qq.com>
2022-04-15 00:03:59 +0800

    jackson

--- src/main/java/Servlet录播学习笔记/PostParameterJson.java
+++ src/main/java/Servlet录播学习笔记/PostParameterJson.java
@@ -1,5 +1,8 @@
 package Servlet录播学习笔记;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,16 +11,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 
+//通过这个类来表示解析后的结果
+class JsonData{
+    public int username;
+    public int passwd;
+}
+
 @WebServlet("/postJson")
 public class PostParameterJson extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setContentType("application/json;charset=utf-8");
+//        resp.setContentType("application/json;charset=utf-8");
 
         String body = readBody(req);
 
-        resp.getWriter().write(body);
+//        使用Jackson来解析
+//        先创建一个Jackson的核心对象
+        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.readValue(转化谁, 转化成啥样)
+//        此处的大致过程为：1.先把body转化为类似HashMap的键值对结构
+//                      2.根据类对象(JsonData.class)获取要转换成啥样的类(都有哪些属性，每个属性的名字)
+//                      3.拿着JsonData.class中的每个属性中名字去body转化处的HashMap里面查
+//                      4.如果查到了，就把查询到的值赋值给JsonData.class里对应的属性。循环进行这个过程.
+//                      5.既然这个过程是要对应名字和body中的key是一样的才可以匹配，那么就要在构造这个类的同时，要整属性和body中要的参数一致！
+        JsonData jsonData = objectMapper.readValue(body, JsonData.class);
+//        resp.getWriter().write(body);
+        resp.getWriter().write(String.format("username : %s , passwd = %s <br>",jsonData.username,jsonData.passwd));
     }
 
     private String readBody(HttpServletRequest req) throws IOException {
@@ -32,6 +52,9 @@ public class PostParameterJson extends HttpServlet {
 //        返回连续的字符串
 //        当前是把整个body视为一个整体的字符串了，实际上是需要解析json格式的body，从里面拿到需要的键值对
 //        更好的办法是直接使用第三方库，Jackson
+//        使用Jackson 需要引入maven依赖
+//        使用Jackson解析Json的时候需要明确把body这个字符串转成一个啥样的对象
+
         return new String(buffer, "utf-8");
     }
 }
