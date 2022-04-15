fa553f897480e1e95ebb9f1093786f759130c898
ningxia2021 <2424917250@qq.com>
2022-04-15 15:21:22 +0800

    HttpServlet req resp 基本功能通过domo实现 学习post的调试方法

--- src/main/java/Servlet录播学习笔记/PostParameter.java
+++ src/main/java/Servlet录播学习笔记/PostParameter.java
@@ -7,6 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * 使用 HttpServletRequest 这个类最常用的，就是获取请求中的参数
+ * 1. 参数是query string 形式，通过req.getParameter() 获取
+ * 2. 参数是body (application/x-www-form-urlencoded) 通过req.getParameter() 获取
+ * 3. 参数是body (application/json) 先把整个body读出来，然后再使用JSON库 jackson来解析，转化成string 最后读出来
+ */
 @WebServlet("/postParameter")
 public class PostParameter extends HttpServlet {
     @Override
--- src/main/java/Servlet录播学习笔记/PostParameterJson.java
+++ src/main/java/Servlet录播学习笔记/PostParameterJson.java
@@ -1,9 +1,7 @@
 package Servlet录播学习笔记;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,32 +10,34 @@ import java.io.IOException;
 import java.io.InputStream;
 
 //通过这个类来表示解析后的结果
-class JsonData{
-    public int username;
+class JsonData {
+    //    这里属性的类型和名字 一定要与data: JSON.stringify(body)中body定义的数据类型和名字一致
+    public String username;
     public int passwd;
 }
 
 @WebServlet("/postJson")
 public class PostParameterJson extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//      发送到客户端响应的内容类型
 //        resp.setContentType("application/json;charset=utf-8");
-
+        resp.setContentType("text/html;charset=utf-8");
         String body = readBody(req);
 
 //        使用Jackson来解析
 //        先创建一个Jackson的核心对象
         ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.readValue(转化谁, 转化成啥样)
 //        此处的大致过程为：1.先把body转化为类似HashMap的键值对结构
 //                      2.根据类对象(JsonData.class)获取要转换成啥样的类(都有哪些属性，每个属性的名字)
 //                      3.拿着JsonData.class中的每个属性中名字去body转化处的HashMap里面查
 //                      4.如果查到了，就把查询到的值赋值给JsonData.class里对应的属性。循环进行这个过程.
 //                      5.既然这个过程是要对应名字和body中的key是一样的才可以匹配，那么就要在构造这个类的同时，要整属性和body中要的参数一致！
+//                          objectMapper.readValue(转化谁, 转化成啥样)
         JsonData jsonData = objectMapper.readValue(body, JsonData.class);
+//        直接write(body) 相当于无差别的把body响应回浏览器
 //        resp.getWriter().write(body);
-        resp.getWriter().write(String.format("username : %s , passwd = %s <br>",jsonData.username,jsonData.passwd));
+        resp.getWriter().write(String.format("JSON 格式获取 ： " + "username : %s , passwd = %d ", jsonData.username, jsonData.passwd)+"</br>");
     }
 
     private String readBody(HttpServletRequest req) throws IOException {
@@ -54,7 +54,6 @@ public class PostParameterJson extends HttpServlet {
 //        更好的办法是直接使用第三方库，Jackson
 //        使用Jackson 需要引入maven依赖
 //        使用Jackson解析Json的时候需要明确把body这个字符串转成一个啥样的对象
-
         return new String(buffer, "utf-8");
     }
 }
